3582eec72f22301a95bf6b0bd5d49c44ab6d12d0
Elnikou <e.elnikou@hackspace.by>
2018-04-20 19:55:46 +0300

    changed first page

---
+++ src/java/controllers/AuthController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import model.dao.UserDAO;
+import model.pojo.User;
+
+import services.AuthService;
+
+/**
+ *
+ * @author 7853j
+ */
+@Controller
+@RequestMapping("/userRegistration.htm")
+@SessionAttributes("user")
+public class AuthController {
+    private AuthService authService;
+
+    @Autowired
+    public void setUserService(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String showUserForm(ModelMap model) {
+        User user = new User();
+        model.addAttribute(user);
+        return "loginPage";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String onSubmit(@ModelAttribute("user") User user) {
+        if (authService.check(user) == 1){
+            return "redirect:userSuccess.htm";
+        }
+        return "loginPage";
+    }
+}
---
+++ src/java/services/AuthService.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import model.pojo.User;
+/**
+ *
+ * @author 7853j
+ */
+public class AuthService {
+    public static String sayHello(String name) {
+        return "Hello " + name + "!";
+    }
+    public int check(User user){
+        
+        return 1;
+    }
+}
